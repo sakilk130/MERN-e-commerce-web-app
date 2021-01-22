24b731ec424ff22aa24d396fbf2dc5489b2796d6
sakilk130 <sakilk130@gmail.com>
2021-01-22 11:16:42 +0600

    front-end--> view products on homeScreen

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import HomeScreen from './screens/HomeScreen';
 function App() {
   return (
     <>
       <Header />
       <main>
         <Container>
-          <h1>Hi, Welcome to Tech-City</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
