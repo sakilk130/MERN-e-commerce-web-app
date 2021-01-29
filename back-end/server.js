8bc40839c646e6acab5e78a30bb3ddc1cb5e75d2
sakilk130 <sakilk130@gmail.com>
2021-01-30 01:45:19 +0600

    connect database

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import products from './data/products.js';
+import connectDB from './config/db.js';
+
 dotenv.config();
 const app = express();
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API is running....');
